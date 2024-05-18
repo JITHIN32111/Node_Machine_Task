092074b621392ef2a2cae1079f02c9a0ccaa3fd9
Jithin C <jith97972@gmail.com>
2024-05-18 12:38:25 +0530

    database connected

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express, { json, urlencoded } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
+import connectDB from "./config/db.js";
 
 dotenv.config();
-
+connectDB();
 
 const app = express();
 
@@ -13,4 +14,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
